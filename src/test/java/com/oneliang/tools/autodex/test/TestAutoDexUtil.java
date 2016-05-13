f8df4a91fb5d7cd800749a3fcba9ceffb54e82f6
oneliang <oneliang@tencent.com>
2016-05-13 16:33:33 +0800

    update test

--- src/test/java/com/oneliang/tools/autodex/test/TestAutoDexUtil.java
+++ src/test/java/com/oneliang/tools/autodex/test/TestAutoDexUtil.java
@@ -1,14 +1,18 @@
 package com.oneliang.tools.autodex.test;
 
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.oneliang.Constant;
 import com.oneliang.thirdparty.asm.util.AsmUtil;
 import com.oneliang.tools.autodex.AutoDexUtil;
 import com.oneliang.util.file.FileUtil;
+import com.oneliang.util.logging.AbstractLogger;
 import com.oneliang.util.logging.BaseLogger;
+import com.oneliang.util.logging.ComplexLogger;
+import com.oneliang.util.logging.FileLogger;
 import com.oneliang.util.logging.Logger;
 import com.oneliang.util.logging.LoggerManager;
 
@@ -21,9 +25,13 @@ public class TestAutoDexUtil {
 	private static final boolean debug=false;
 
 	public static void main(String[] args) throws Exception{
-		LoggerManager.registerLogger(AutoDexUtil.class, new BaseLogger(Logger.Level.INFO));
-		LoggerManager.registerLogger(AsmUtil.class, new BaseLogger(Logger.Level.VERBOSE));
-		System.setOut(new PrintStream(new FileOutputStream("/D:/mainDex.txt")));
+		List<AbstractLogger> loggerList=new ArrayList<AbstractLogger>();
+		loggerList.add(new BaseLogger(Logger.Level.INFO));
+		loggerList.add(new FileLogger(Logger.Level.VERBOSE,new File("/D:/a.txt")));
+		Logger logger=new ComplexLogger(Logger.Level.ERROR, loggerList);
+		LoggerManager.registerLogger(AutoDexUtil.class, logger);
+		LoggerManager.registerLogger(AsmUtil.class, logger);
+//		System.setOut(new PrintStream(new FileOutputStream("/D:/mainDex.txt")));
 		FileUtil.deleteAllFile(outputDirectory);
 		AutoDexUtil.autoDex(allClassesJar, androidManifestFullFilename, Arrays.asList(mainDexOtherClasses.split(Constant.Symbol.COMMA)), outputDirectory, debug);
 	}
